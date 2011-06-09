e5fec90eadbde7ad8c067e9a845bac23e9cd5522
Manu Gupta <manugupt1@opensuse.org>
2011-06-09 17:54:20 +0530

    Dynamic Loading of modules

--- src/main.cxx
+++ src/main.cxx
@@ -2,11 +2,75 @@
 
 #include<locale.h>
 #include<libintl.h>
+#include<dirent.h>
+#include<sys/types.h>
+#include<string.h>
+#include<list>
 
-#include "ui/yui.h"
+extern "C"{
+#include<augeas.h>
+}
+
 #include "ui/yuifactory.h"
 
 #define _(STRING) gettext(STRING)
+
+
+class Init{
+
+	DIR * dp;
+	struct dirent * ep;
+	
+	augeas * aug;
+	char  *root,*loadpath;
+	unsigned int flag;
+	const char *value;
+	int err;
+	char* getEntry;
+	protected:
+		void makeEntry(char s1[],char s2[],char s3[]){
+			strcpy(getEntry,s1);
+			strcat(getEntry,s2);
+			strcat(getEntry,s3);
+		}
+
+	public:
+	Init(){
+		aug=NULL;root=NULL;loadpath=NULL;flag=0;
+		UI::YUIFactory * factory = new UI::YUIFactory();
+		UI::yDialog * dialog = factory->createDialog(30,10);
+		UI::yVLayout * mainLayout = factory->createVLayout(dialog);
+		dp = opendir("/usr/share/sax3/modules.d/");
+		aug = aug_init(root,loadpath,flag);
+		aug_set(aug,"/augeas/load/Desktop/incl[last()+1]","/usr/share/sax3/modules.d/*");
+		err = aug_load(aug);
+		
+		if(dp!=NULL){
+			while(ep = readdir(dp)){
+				if(strcmp(ep->d_name,".") && strcmp(ep->d_name,"..")){
+				
+					UI::yHLayout * hLayout = factory->createHLayout(mainLayout);
+					getEntry = new char[300];
+					makeEntry("/files/usr/share/sax3/modules.d/",ep->d_name,"/*/Icon");
+					err = aug_get(aug,getEntry,&value);
+					if(err==1)
+					UI::yImage * image = factory->createImage(hLayout,value);
+					delete getEntry;
+
+					getEntry = new char[100];
+					makeEntry("/files/usr/share/sax3/modules.d/",ep->d_name,"/*/Name");
+					err = aug_get(aug,getEntry,&value);
+					if(err==1)
+					UI::yLabel * button = factory->createButton(hLayout,value);
+					delete getEntry;	
+				}
+			}
+
+		}	
+		dialog->event();
+		
+	}
+};
 
 int main(){
 	
@@ -14,7 +78,8 @@ int main(){
 	bindtextdomain("sax3","/usr/share/locale");
 	textdomain("sax3");
 
-	UI::YUIFactory * factory = new UI::YUIFactory();
+	new Init();
+/*	UI::YUIFactory * factory = new UI::YUIFactory();
 	UI::yDialog * dialog = factory->createDialog(30,10);
 
 	UI::yVLayout * mainLayout = factory->createVLayout(dialog);
@@ -37,7 +102,7 @@ int main(){
 	UI::yImage * TouchPadImage = factory->createImage(TouchPadLayout,"/usr/share/sax3/res/touchpad.png");
 	UI::yPushButton * TouchPadButton = factory->createPushButton(TouchPadLayout,_("&Touch Pad"));
 
-	dialog->event();
+	dialog->event();*/
 	return 0;
 }
 
