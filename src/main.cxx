a77fad1d8119ab24a0547160411a0a574ef70545
Manu Gupta <manugupt1@gmail.com>
2011-07-01 15:44:15 +0530

    execlp works

--- src/main.cxx
+++ src/main.cxx
@@ -94,7 +94,7 @@ class Init{
 				string temp = button[i]->value();
 				temp.erase(temp.find('&'),temp.find('&'));
 				cout<<execs[i]<<endl;
-				err = execl("sax3-keyboard","");
+				err = execlp(execs[i].c_str(),"");
 				break;
 			}
 		}
@@ -108,30 +108,6 @@ int main(){
 	textdomain("sax3");
 
 	new Init();
-/*	UI::YUIFactory * factory = new UI::YUIFactory();
-	UI::yDialog * dialog = factoryntWidget->createDialog(30,10);
-
-	UI::yVLayout * mainLayout = factory->createVLayout(dialog);
-
-	UI::yLabel * label1 = factory->createLabel(mainLayout,_("SaX3 Control Center"));
-
-	UI::yHLayout * KeyBoardLayout = factory->createHLayout(mainLayout);
-	UI::yImage * KeyBoardImage = factory->createImage(KeyBoardLayout,"/usr/share/sax3/res/keyboard.png");
-	UI::yPushButton * KeyBoardButton = factory->createPushButton(KeyBoardLayout,_("&Key Board"));
-	
-	UI::yHLayout * MouseLayout = factory->createHLayout(mainLayout);
-	UI::yImage * MouseImage = factory->createImage(MouseLayout,"/usr/share/sax3/res/mouse.png");
-	UI::yPushButton * MouseButton = factory->createPushButton(MouseLayout,_("&Mouse"));
-
-	UI::yHLayout * MonitorLayout = factory->createHLayout(mainLayout);
-	UI::yImage * MonitorImage = factory->createImage(MonitorLayout,"/usr/share/sax3/res/monitor.png");
-	UI::yPushButton * MonitorButton = factory->createPushButton(MonitorLayout,_("Mo&nitor"));
-
-	UI::yHLayout * TouchPadLayout = factory->createHLayout(mainLayout);
-	UI::yImage * TouchPadImage = factory->createImage(TouchPadLayout,"/usr/share/sax3/res/touchpad.png");
-	UI::yPushButton * TouchPadButton = factory->createPushButton(TouchPadLayout,_("&Touch Pad"));
-
-	dialog->event();*/
 	return 0;
 }
 
